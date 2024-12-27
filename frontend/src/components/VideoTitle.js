61670b18f5b9d45cdced7fd53b375f0d818ac847
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-27 11:41:17 +0530

    Fetch trailer video

--- frontend/src/components/VideoTitle.js
+++ frontend/src/components/VideoTitle.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const VideoTitle = ({title, overview}) => {
+const VideoTitle = ({ title, overview }) => {
   return (
-    <div className='text-black pt-36 px-12 font-inter'>
-      <h1 className='text-6xl font-bold'>{title}</h1>
-      <p className='py-6 text-lg w-1/3'>{overview}</p>
-      <div className=''>
-        <button className='bg-gray-500 p-4 px-12 text-xl rounded-sm bg-opacity-50'>Play</button>
-        <button className='bg-gray-500 mx-3 p-4 px-12 text-xl rounded-sm bg-opacity-50'>More Info</button>
+    <div className="text-black pt-36 px-12 font-inter">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/3">{overview}</p>
+      <div className="">
+        <button className="bg-gray-500 p-4 px-12 text-xl rounded-sm bg-opacity-50">
+          Play
+        </button>
+        <button className="bg-gray-500 mx-3 p-4 px-12 text-xl rounded-sm bg-opacity-50">
+          More Info
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoTitle
+export default VideoTitle;
