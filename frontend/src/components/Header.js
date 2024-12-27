cfcfac1d53a7e0f655f1d160dac4aa0c58bbd4f4
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-27 17:07:11 +0530

    created GPT search toggle button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -19,6 +20,12 @@ const Header = () => {
         navigate("/error");
       });
   };
+
+  const handleGptSearchClick = () => {
+    // toggle GPT search
+    dispatch(toggleGptSearchView())
+
+  }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -45,7 +52,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between font-inter">
       <img
         className="w-44"
         src={LOGO}
@@ -54,6 +61,7 @@ const Header = () => {
 
       {user && (
         <div className="flex p-2">
+          <button className="py-2 px-4 m-2 bg-red-700 text-white rounded-md mx-4 my-2" onClick={handleGptSearchClick}>GPT Search</button>
           <img className="w-8 h-8" src={user?.photoURL} alt="user Icon" />
 
           <button onClick={handleSignOut} className="font-bold text-white">
