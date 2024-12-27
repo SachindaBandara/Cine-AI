d9637a74af0350041c146a356bd8e8268b783085
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-27 13:16:03 +0530

    add

--- frontend/src/components/MainContainer.js
+++ frontend/src/components/MainContainer.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import {useSelector} from "react-redux"
-import VideoBackground from './VideoBackground'
-import VideoTitle from './VideoTitle'
+import React from "react";
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const MainContainer = () => {
-    const movies = useSelector(store => store.movie?.nowPlayingMovies);
+  const movies = useSelector((store) => store.movie?.nowPlayingMovies);
 
-    if(!movies) return;
-    const mainMovie = movies[0];
+  if (!movies) return;
+  const mainMovie = movies[0];
 
-
-    const {original_title, overview, id} = mainMovie;
+  const { original_title, overview, id } = mainMovie;
 
   return (
     <div>
-        <VideoTitle title={original_title} overview={overview}/>
-      <VideoBackground movieId={id}/>
-     
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
